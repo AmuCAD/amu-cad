54141c1a2614f2834dcf454e49393b9e4bf17410
changyun-go <aeiou75three@gmail.com>
2022-07-11 19:04:59 +0900

    ♻ 컴포넌트 로직 분리

--- src/components/model-control/Extrude/index.jsx
+++ src/components/model-control/Extrude/index.jsx
@@ -1,16 +1,11 @@
 import { useEffect, useState, useRef, useMemo } from "react";
-import { CSG } from "three-csg-ts";
-
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { CSG } from "three-csg-ts";
 
 import useStore from "../../../store";
 import getPosition from "../../../utils/getPosition";
-import SelectionHelper from "../../common/SelectionHelper";
 
-function Extrude() {
-  const baseCoordinate = useStore(state => state.baseCoordinate);
-  const extrudeSize = useStore(state => state.extrudeSize);
+function Extrude({ model, setModel }) {
   const [extrudeShape, setExtrudeShape] = useStore(state => [
     state.extrudeShape,
     state.setExtrudeShape,
@@ -19,35 +14,14 @@ function Extrude() {
     state.isConfirm,
     state.setIsConfirm,
   ]);
-  const [activeFunction, setActiveFunction] = useStore(state => [
-    state.activeFunction,
-    state.setActiveFunction,
-  ]);
-  const setBaseCoordinate = useStore(state => state.setBaseCoordinate);
+  const baseCoordinate = useStore(state => state.baseCoordinate);
+  const setActiveFunction = useStore(state => state.setActiveFunction);
   const operationType = useStore(state => state.operationType);
-  const importFile = useStore(state => state.importFile);
-  const [model, setModel] = useStore(state => [state.model, state.setModel]);
-  const isSketchMode = useStore(state => state.isSketchMode);
+  const extrudeSize = useStore(state => state.extrudeSize);
   const isForwardDirection = useStore(state => state.isForwardDirection);
   const [extrudeSettings, setExtrudeSettings] = useState({});
-  const [prevPoint, setPrevPoint] = useState(null);
-  const [sameCoordinate, setSameCoordinate] = useState(null);
-  const [mouse, setMouse] = useState(null);
-  const [dataToGetAngle, setDataToGetAngle] = useState([]);
-  const [angle, setAngle] = useState([0, 0, 0]);
 
   const ref = useRef(null);
-
-  useEffect(() => {
-    const gltfLoader = new GLTFLoader();
-    const blobUrl = URL.createObjectURL(
-      new Blob([importFile], { type: "text.plain" }),
-    );
-
-    gltfLoader.load(blobUrl, gltf => {
-      setModel(gltf.scene);
-    });
-  }, [importFile]);
 
   useEffect(() => {
     setExtrudeSettings({
@@ -94,86 +68,9 @@ function Extrude() {
       : getPosition(key, value, extrudeSize).position;
   }, [position, extrudeSize, isForwardDirection]);
 
-  const findSameCoordinate = (prevPoint, currentPoint, sameCoordinate) => {
-    for (const prop in prevPoint) {
-      if (!prevPoint.hasOwnProperty(prop)) continue;
-      if (prevPoint[prop] === currentPoint[prop]) {
-        return { [prop]: prevPoint[prop] };
-      }
-    }
-
-    return sameCoordinate;
-  };
-
-  useEffect(() => {
-    if (prevPoint && mouse) {
-      if (dataToGetAngle.length === 60) {
-        const countHash = {};
-
-        dataToGetAngle.forEach(elem => {
-          if (countHash[elem]) {
-            countHash[elem]++;
-          } else {
-            countHash[elem] = 1;
-          }
-        });
-
-        let angle = "";
-        let max = 0;
-
-        for (const key in countHash) {
-          if (max < countHash[key]) {
-            angle = key;
-            max = countHash[key];
-          }
-        }
-
-        const angleIndexes = [];
-
-        dataToGetAngle.forEach((elem, index) => {
-          if (elem === Number(angle)) {
-            angleIndexes.push(index);
-          }
-        });
-
-        for (const index of angleIndexes) {
-          if (index === 3 || index === 6 || index === 9) {
-            setAngle([Math.PI / 2 - angle, 0, 0]);
-            break;
-          } else if (index === 4 || index === 7 || index === 10) {
-            setAngle([Math.PI / 2, angle, 0]);
-            break;
-          } else if (index === 5 || index === 8 || index === 11) {
-            setAngle([0, angle - Math.PI / 2, 0]);
-            break;
-          }
-        }
-      }
-
-      setDataToGetAngle(data => {
-        if (data.length === 60) {
-          data.splice(0, 3);
-        }
-
-        data.push(
-          Math.round(
-            Math.atan2(mouse.y - prevPoint.y, mouse.z - prevPoint.z) * 100,
-          ) / 100,
-          Math.round(
-            Math.atan2(mouse.y - prevPoint.y, mouse.x - prevPoint.x) * 100,
-          ) / 100,
-          Math.round(
-            Math.atan2(mouse.x - prevPoint.x, mouse.z - prevPoint.z) * 100,
-          ) / 100,
-        );
-        return data;
-      });
-    }
-  }, [prevPoint]);
-
   return (
     <>
-      {activeFunction === "EXTRUDE" && extrudeShape && (
+      {extrudeShape && (
         <mesh ref={ref} position={extrudePosition} rotation={rotation}>
           <extrudeGeometry
             attach="geometry"
@@ -187,40 +84,6 @@ function Extrude() {
             transparent
           />
         </mesh>
-      )}
-      {model && (
-        <primitive
-          onPointerMove={e => {
-            if (isSketchMode && !baseCoordinate) {
-              e.stopPropagation();
-              setSameCoordinate(
-                findSameCoordinate(prevPoint, e.point, sameCoordinate),
-              );
-            }
-
-            setMouse(e.point);
-            setPrevPoint(mouse);
-          }}
-          onPointerOut={() => {
-            setMouse(null);
-          }}
-          onClick={e => {
-            if (isSketchMode && !baseCoordinate) {
-              e.stopPropagation();
-              setBaseCoordinate(sameCoordinate);
-            }
-          }}
-          object={model}
-          material={
-            new THREE.MeshBasicMaterial({
-              color: "white",
-              side: THREE.DoubleSide,
-            })
-          }
-        />
-      )}
-      {isSketchMode && !baseCoordinate && mouse && (
-        <SelectionHelper mouse={mouse} angle={angle} />
       )}
     </>
   );
